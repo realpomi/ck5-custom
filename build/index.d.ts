bc7f4e179274a1f403c0a4171e32130e6248214f
Realpomi <realpomi@gmail.com>
2021-03-30 23:01:33 +0900

    add typescipr module

---
+++ build/index.d.ts
@@ -0,0 +1,5 @@
+declare module '@ckeditor/ckeditor5-react' {
+    export default class Ckeditor extends React.Component {
+        constructor({disabled}: {disabled?: boolean}) // this part needs to be fullfilled with your needs
+    }
+}
